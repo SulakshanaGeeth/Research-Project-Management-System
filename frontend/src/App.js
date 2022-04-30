6f1b8926d9aabb21a581eed66c3e6bb96906bcbd
Sahan Kumarasiri <sahankumarasiri@1billiontech.com>
2022-04-30 12:54:43 +0530

    Frontend Login Register Implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import ResetPassword from "./components/Register/ResetPassword";
+import PageNotFound from "./routes/PageNotFound";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/passwordreset/:resetToken"
+            element={<ResetPassword />}
+          />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
