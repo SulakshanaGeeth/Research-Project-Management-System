4284e613e1f6ed097a4c8883ff9a11d2113d2939
IT19003160 <it19003160@my.sliit.lk>
2022-05-21 12:41:23 +0530

    Scroll To Top Implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 // Login Register Reset Imports Goes Here
 
@@ -14,6 +15,26 @@ import PrivateRoute from "./routes/PrivateRoute";
 import SupervisorDashboard from "./components/Staff/Supervisor/Dashboard";
 
 const App = () => {
+  // The back-to-top button is hidden at the beginning
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 100) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  // This function will scroll the window to the top
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // for smoothly scrolling
+    });
+  };
   return (
     <>
       <Router>
@@ -48,6 +69,15 @@ const App = () => {
           />
         </Routes>
       </Router>
+      <>
+        {/* React Fragment */}
+        {showButton && (
+          <button onClick={scrollToTop} className="back-to-top">
+            <i class="fa fa-arrow-circle-up" aria-hidden="true"></i>
+          </button>
+        )}
+        {/* &#8679; is used to create the upward arrow */}
+      </>
     </>
   );
 };
