3d0bb20978f5bcee5f956a2c6e286ecfef48ac03
IT19003160 <it19003160@my.sliit.lk>
2022-05-21 17:27:43 +0530

    Research Topic Backend Implementation Done

---
+++ BACKEND/models/ResearchTopic/index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Topic = new Schema({
+  topicId: String,
+  topicName: String,
+  userEmail: String,
+  topicCat: String,
+  faculty: String,
+  members: Number,
+  date: Date,
+  lastModified: Date,
+  status: String,
+  acceptOrRejectBy: String,
+});
+
+const newTopic = mongoose.model("research-topic", Topic);
+module.exports = newTopic;
