fe5251c74720f4892108849b30363f2dfb6f7543
ThamaliiNirmala <tnirmalaadasooriya@gmail.com>
2022-05-24 14:00:28 +0530

    Minor changes in controllers auth

--- BACKEND/controllers/auth.js
+++ BACKEND/controllers/auth.js
@@ -202,7 +202,7 @@ exports.get = async (req, res) => {
 
 exports.getById = async (req, res) => {
   const { id } = req.params;
-  await User.findOne({ empId: id })
+  await User.findById(id)
     .then((user) => res.json(user))
     .catch((err) => res.status(500).json({ err }));
 };
